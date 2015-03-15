e8382e8deadc042c5dbfd98c9e353e555c17d8b6
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-15 08:50:06 +0100

    Lint btsync::service

--- manifests/service.pp
+++ manifests/service.pp
@@ -19,7 +19,7 @@ class btsync::service {
   $ensure = $::btsync::start ? { true => running, default => stopped }
 
   service{'btsync':
-    ensure    => $ensure,
-    enable    => $::btsync::enable,
+    ensure => $ensure,
+    enable => $::btsync::enable,
   }
 }
