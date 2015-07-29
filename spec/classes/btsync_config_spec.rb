debd298551aaac146352645a816baaf48cadb4b4
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-07-29 16:44:10 +0200

    Fix unit tests

--- spec/classes/btsync_config_spec.rb
+++ spec/classes/btsync_config_spec.rb
@@ -11,7 +11,7 @@ describe 'btsync::config' do
       :foo => 'bar',
     } }
 
-    it { expect { is_expected.to compile }.to raise_error(/Invalid parameter: 'foo'/) }
+    it { expect { is_expected.to compile }.to raise_error(/Invalid parameter/) }
   end
 
 end
